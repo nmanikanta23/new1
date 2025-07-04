6e964fe2c9086870a098e70d9cbf7fb63bd8f5d4
nmanikanta23 <125890299+nmanikanta23@users.noreply.github.com>
2025-07-04 21:14:26 +0530

    Update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vp&vig*ut4-enrrdbb(4gl+uus$90yuzqalo)=l$dscq(7u&x7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
